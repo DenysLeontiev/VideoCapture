666bbbfd0e5f47eb8c11218f5532584ee2a10bfe
Denys Leon <leonden0046@gmail.com>
2024-01-22 09:22:25 +0200

    Add project files.

---
+++ VideoCapture/Controllers/VideoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace VideoCapture.Controllers
+{
+    public class VideoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveRecordFile()
+        {
+            if(Request.Form.Files.Any())
+            {
+                var file = Request.Form.Files["video-blob"];
+                string uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UploadedFiles");
+                string fileName = Guid.NewGuid().ToString() + "_" + file.FileName + ".webm";
+                string uploadPath = Path.Combine(uploadFolder, fileName);
+                await file.CopyToAsync(new FileStream(uploadPath, FileMode.Create));
+            }
+
+            return Json(HttpStatusCode.OK);
+        }
+    }
+}
